54b5e4f981d28b4c1967ddfc8ee8eef03c4797b4
Yusuf Deniz DOĞAN <114568545+yusufdenizdd@users.noreply.github.com>
2025-12-09 12:39:01 +0300

    Smooth Moves

---
+++ Assets/Scripts/Tools/Movable.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+//using System.Numerics;
+using UnityEngine;
+
+// This script will allow any game object to be moved smoothly
+// from its current position to a new target position at a speed
+// set in the inspector using a coroutine.
+// 
+// The speed must be a positive number
+// 
+// You can see if the objects is currently moving using Idle
+// 
+// There is an Easing function to alter the speed of the animation over time
+// 
+
+
+public class Movable : MonoBehaviour
+{
+    private Vector3 from;
+    private Vector3 to;
+    private float howfar;
+    private bool idle = true;
+    public bool Idle
+    {
+        get
+        {
+            return idle;
+        }
+    }
+    [SerializeField] private float speed = 1;
+
+    // coroutine move from current position to new position
+    public IEnumerator MoveToPosition(Vector3 targetPosition)
+    {
+        if (speed <= 0)
+        {
+            Debug.LogWarning("Speed must be a positive number.");
+        }
+        from = transform.position;
+        to = targetPosition;
+        howfar = 0;
+        idle = false;
+
+
+
+        do
+        {
+            howfar += speed * Time.deltaTime;
+            if (howfar > 1)
+            {
+                howfar = 1;
+            }
+            transform.position = Vector3.LerpUnclamped(from, to, Easing(howfar));
+            yield return null;
+        } while (howfar != 1);
+        idle = true;
+
+
+
+    }
+    private float Easing(float t)
+    {
+        return t * t;
+    }
+}
